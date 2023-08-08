271e4e7e1c4a270a48e57fb40427ace77cf89404
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2023-08-08 18:57:58 -0400

    initial

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Settings() {
+    return (<div>
+        yo yo yo yo 
+    </div>)
+}
+
+function App() {
+    return (
+    <div>
+        <Settings></Settings>
+    </div>)
+}
+
+export default App;
